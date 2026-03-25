2b51bc0afa9d1c0a73ef174a19542f75db7dd384
MarksVale <124680184+MarksVale@users.noreply.github.com>
2026-03-25 18:33:34 +0200

    fix: always show Cloudinary photos on river cards and pages

--- src/components/RiverCard.tsx
+++ src/components/RiverCard.tsx
@@ -24,7 +24,4 @@ export function RiverCard({ slug, name, gradient, routeCount, description, regio
           <span>{region}</span>
           <span style={{ marginLeft: 'auto' }}>{bookable ? 'Book Online' : 'Call to Book'}</span>
         </div>
-      </div>
-    </Link>
-  )
-}
+      </div
