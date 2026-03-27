9d4e80299ec85006bb255084a9aee5334693edea
MarksVale <124680184+MarksVale@users.noreply.github.com>
2026-03-27 16:55:59 +0200

    Switch to localized Link in page.tsx

--- src/app/[locale]/fleet/page.tsx
+++ src/app/[locale]/fleet/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import { Link } from '@/i18n/navigation'
 import { getBoats } from '@/lib/airtable'
 import BoatCardWithModal from '@/components/BoatCardWithModal'
 import PageHero from '@/components/PageHero'
