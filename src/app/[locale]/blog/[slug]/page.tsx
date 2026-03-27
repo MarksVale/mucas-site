a124a80ab48339c54eba21eea80e35a53906afd0
MarksVale <124680184+MarksVale@users.noreply.github.com>
2026-03-27 16:55:56 +0200

    Switch to localized Link in page.tsx

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import { Link } from '@/i18n/navigation'
 import type { Metadata } from 'next'
 import { buildAlternates, buildOpenGraph, twitterCard, SITE_NAME } from '@/lib/seo'
 import { BLOG_POSTS, getPost } from '@/content/blog/posts'
