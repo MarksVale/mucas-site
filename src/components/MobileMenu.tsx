e77a54b7186a1782dfa2436b60d6b8aa134da158
MarksVale <124680184+MarksVale@users.noreply.github.com>
2026-03-27 16:55:50 +0200

    Switch to localized Link in MobileMenu.tsx

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react'
-import Link from 'next/link'
+import { Link } from '@/i18n/navigation'
 import { useTranslations } from 'next-intl'
 import { IconAccount } from './Icons'
 import { LangToggle } from './LangToggle'
