a6222795ba4d0acc5f9e7ce1b13f7b6626389f2a
MarksVale <124680184+MarksVale@users.noreply.github.com>
2026-03-27 16:55:55 +0200

    Switch to localized Link in page.tsx

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import { Link } from '@/i18n/navigation'
 import PageHero from '@/components/PageHero'
 import type { Metadata } from 'next'
 import { buildAlternates, buildOpenGraph, twitterCard, SITE_NAME } from '@/lib/seo'
